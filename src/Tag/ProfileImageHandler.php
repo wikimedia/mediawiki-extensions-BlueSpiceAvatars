4514656182d9039bd4e41f30c9b0f0711cad671a
pwirth <wirth@hallowelt.biz>
2019-06-18 10:04:17 +0200

    Implement missing tag handler for profileimage tag. ERM:15575
    => Needs cherry-pick to REL1_31_dev and REL1_31_dev
    
    Change-Id: I8f81347b32df7bb50b8811a3f0f538e92938d1a9

---
+++ src/Tag/ProfileImageHandler.php
@@ -0,0 +1,116 @@
+<?php
+
+namespace BlueSpice\Avatars\Tag;
+
+use MWException;
+use Parser;
+use PPFrame;
+use Html;
+use User;
+use BlueSpice\RendererFactory;
+use BlueSpice\Renderer\Params;
+use BlueSpice\Renderer\UserImage;
+use BlueSpice\Tag\Handler;
+use BlueSpice\DynamicFileDispatcher\UrlBuilder;
+use BlueSpice\DynamicFileDispatcher\Params as DFDParams;
+use BlueSpice\Avatars\DynamicFileDispatcher\UserProfileImage;
+
+class ProfileImageHandler extends Handler {
+
+	/**
+	 *
+	 * @var UrlBuilder
+	 */
+	protected $dfdUrlBuilder = null;
+
+	/**
+	 *
+	 * @var RendererFactory
+	 */
+	protected $rendererFactory = null;
+
+	/**
+	 *
+	 * @param string $processedInput
+	 * @param array $processedArgs
+	 * @param Parser $parser
+	 * @param PPFrame $frame
+	 * @param UrlBuilder $dfdUrlBuilder
+	 * @param RendererFactory $rendererFactory
+	 */
+	public function __construct( $processedInput, array $processedArgs, Parser $parser,
+		PPFrame $frame, UrlBuilder $dfdUrlBuilder, RendererFactory $rendererFactory ) {
+		parent::__construct( $processedInput, $processedArgs, $parser, $frame );
+		$this->dfdUrlBuilder = $dfdUrlBuilder;
+		$this->rendererFactory = $rendererFactory;
+	}
+
+	/**
+	 *
+	 * @return string
+	 */
+	public function handle() {
+		$this->processedArgs['username'] = isset( $this->processedArgs['username'] )
+			? $this->processedArgs['username']
+			: '';
+		$this->processedArgs['width'] = isset( $this->processedArgs['width'] )
+			? (int)$this->processedArgs['width']
+			: 32;
+		$this->processedArgs['height'] = isset( $this->processedArgs['height'] )
+			? (int)$this->processedArgs['height']
+			: 32;
+		$this->processedArgs['raw'] = isset( $this->processedArgs['raw'] )
+			&& $this->processedArgs['raw']
+			? true
+			: false;
+
+		if ( $this->processedArgs['raw'] === true ) {
+			return $this->handleRaw();
+		}
+		$user = User::newFromName( $this->processedArgs['username'] );
+		if ( !$user ) {
+			$msg = \Message::newFromKey(
+				'bs-avatars-tag-profileimage-error-invalidusername'
+			);
+			throw new MWException( $msg );
+		}
+		$params = [
+			UserImage::PARAM_HEIGHT => $this->processedArgs['height'],
+			UserImage::PARAM_WIDTH => $this->processedArgs['width'],
+			UserImage::PARAM_USER => $user,
+			UserImage::PARAM_CLASS => 'bs-avatars-userimage-tag'
+		];
+		return $this->rendererFactory->get(
+			'userimage',
+			new Params( $params )
+		)->render();
+	}
+
+	/**
+	 *
+	 * @return string
+	 */
+	protected function handleRaw() {
+		$params = [
+			DFDParams::MODULE => UserProfileImage::MODULE_NAME,
+			UserProfileImage::USERNAME => $this->processedArgs['username'],
+			UserProfileImage::WIDTH => $this->processedArgs['width'],
+			UserProfileImage::HEIGHT => $this->processedArgs['height']
+		];
+
+		$url = $this->dfdUrlBuilder->build( new DFDParams( $params ) );
+
+		$html = Html::element( 'img', [
+			'src' => $url,
+			'alt' => wfMessage(
+				'bs-avatars-tag-userimage-img-alt',
+				$this->processedArgs['username']
+			)->text(),
+			'class' => 'bs-avatars-userimage-tag',
+			'width' => $this->processedArgs['width'],
+			'height' => $this->processedArgs['height']
+		] );
+
+		return $html;
+	}
+}
