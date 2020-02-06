f61b98682154db5e319c93a2a559589fe62a5d19
pwirth <wirth@hallowelt.biz>
2020-02-06 10:31:08 +0100

    Fix handling of external url as user profile image
    ERM:18148
    [3.1.x]
    
    Change-Id: Ifc62d6bfbda95447c061e60a0dbb86020ff4c844

--- src/DynamicFileDispatcher/ImageExternal.php
+++ src/DynamicFileDispatcher/ImageExternal.php
@@ -2,7 +2,28 @@
 
 namespace BlueSpice\Avatars\DynamicFileDispatcher;
 
+use BlueSpice\DynamicFileDispatcher\Module;
+use MediaTransformError;
+use ThumbnailImage;
+
 class ImageExternal extends Image {
+
+	/**
+	 *
+	 * @var string
+	 */
+	protected $src = '';
+
+	/**
+	 *
+	 * @param Module $dfd
+	 * @param ThumbnailImage|MediaTransformError|bool $thumb
+	 * @param string $src
+	 */
+	public function __construct( Module $dfd, $thumb, $src ) {
+		parent::__construct( $dfd, $thumb );
+		$this->src = $src;
+	}
 
 	/**
 	 * Sets the headers for given \WebResponse
