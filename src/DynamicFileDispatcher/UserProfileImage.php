f67d05821e2db8a63d186cd0ab4d7c3459662fa4
pwirth <wirth@hallowelt.biz>
2020-02-06 11:50:39 +0100

    Quck fix broken thumbs, when requested dimensions bigger then source dimentions
    ERM:18148
    [3.1.x]
    
    Change-Id: I68b70b6ad345fb22d29401e1963758e10476b055

--- src/DynamicFileDispatcher/UserProfileImage.php
+++ src/DynamicFileDispatcher/UserProfileImage.php
@@ -5,6 +5,7 @@ namespace BlueSpice\Avatars\DynamicFileDispatcher;
 use BlueSpice\Avatars\Generator;
 use BlueSpice\DynamicFileDispatcher\UserProfileImage as UPI;
 use BlueSpice\DynamicFileDispatcher\UserProfileImage\AnonImage;
+use File;
 
 class UserProfileImage extends UPI {
 
@@ -59,9 +60,15 @@ class UserProfileImage extends UPI {
 	 */
 	protected function getThumbnailImageFile( $file, $widthName, $heightName ) {
 		$params = [ 'width' => $this->params[ $widthName ] ];
+		if ( $file->getHeight() > $params[ 'width' ] ) {
+			$params[ 'width' ] = $file->getWidth() - 1;
+		}
 		$height = $this->params[ $heightName ];
 		if ( $height != -1 ) {
 			$params['height'] = $height;
+		}
+		if ( isset( $params['height'] ) && $params['height'] > $file->getHeight() ) {
+			$params['height'] = $file->getHeight() - 1;
 		}
 		return new Image( $this, $file->transform( $params ) );
 	}
