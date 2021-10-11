46bfa491a79d5510a1fe76bf3a9d86397c03effa
Alexander Vorwerk <alexander.vorwerk@stud.uni-goettingen.de>
2021-10-11 13:46:37 +0200

    Add visibility to constants
    We support PHP 7.3.19 or later, so we should declare the visibility of
    constants.
    
    Bug: T237840
    Change-Id: I839b5a9d566a40894baaef424bffbf64cb45f6b3

--- src/Config.php
+++ src/Config.php
@@ -3,6 +3,6 @@
 namespace BlueSpice\Avatars;
 
 class Config {
-	const DEFAULT_SIZE = 'AvatarsDefaultSize';
-	const GENERATOR = 'AvatarsGenerator';
+	public const DEFAULT_SIZE = 'AvatarsDefaultSize';
+	public const GENERATOR = 'AvatarsGenerator';
 }
