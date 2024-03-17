98d37fc8530264b2950b01e3bddfe8f703dc44c0
libraryupgrader <tools.libraryupgrader@tools.wmflabs.org>
2024-03-17 14:08:09 +0000

    build: Updating mediawiki/mediawiki-codesniffer to 43.0.0
    Change-Id: I5013671d7f01c8872485bc98c2be00919bc00889

--- src/ConfigDefinition/AvatarsGenerator.php
+++ src/ConfigDefinition/AvatarsGenerator.php
@@ -13,7 +13,7 @@ class AvatarsGenerator extends \BlueSpice\ConfigDefinition\ArraySetting {
 	public function getPaths() {
 		return [
 			static::MAIN_PATH_FEATURE . '/' . static::FEATURE_PERSONALISATION . '/BlueSpiceAvatars',
-			static::MAIN_PATH_EXTENSION . '/BlueSpiceAvatars/' . static::FEATURE_PERSONALISATION ,
+			static::MAIN_PATH_EXTENSION . '/BlueSpiceAvatars/' . static::FEATURE_PERSONALISATION,
 			static::MAIN_PATH_PACKAGE . '/' . static::PACKAGE_FREE . '/BlueSpiceAvatars',
 		];
 	}
