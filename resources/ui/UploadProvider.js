5fdbe3203124b84e1c826c22b5d493c4d62e2c2b
hslater <hamish.slater@hallowelt.com>
2025-05-06 18:31:18 +1200

    PHPCS eslint
    ERM20742
    
    [REL1_43, REL1_43-5.0.x, master]
    
    Change-Id: Ib8d17a8dbebba57bbd295ac174e1a237fb392c17
    (cherry picked from commit 5180f566503716d6bb0582174d11227684014d41)

--- resources/ui/UploadProvider.js
+++ resources/ui/UploadProvider.js
@@ -1,16 +1,16 @@
 bs.util.registerNamespace( 'bs.avatars.ui' );
-console.log( "LOAD" );
-bs.avatars.ui.UploadProvider = function() {
+
+bs.avatars.ui.UploadProvider = function () {
 	bs.avatars.ui.UploadProvider.parent.call( this );
 };
 
 OO.inheritClass( bs.avatars.ui.UploadProvider, ext.userProfile.ui.ProfileImageProvider );
 
-bs.avatars.ui.UploadProvider.prototype.getLabel = function() {
+bs.avatars.ui.UploadProvider.prototype.getLabel = function () {
 	return mw.msg( 'bs-avatars-file-upload-fieldset-title' );
 };
 
-bs.avatars.ui.UploadProvider.prototype.getDialog = function() {
+bs.avatars.ui.UploadProvider.prototype.getDialog = function () {
 	return new bs.avatars.ui.UploadImageDialog( {} );
 };
 
