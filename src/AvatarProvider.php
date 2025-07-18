b3c34ba11f4acc788118b85dce22ee2befc4b34c
Robert Vogel <vogel@hallowelt.biz>
2025-07-18 14:18:26 +0200

    Check for `null`
    `BlueSpice\Avatars\Generator::getAvatarFile` can return `null`
    
    ERM38583
    
    [5.1.x]
    
    Change-Id: I7ef4d3aa9a72b4f31f5d5f5fec854adb6102a7c2
    (cherry picked from commit 835dab68c9e69c993929c32e528c4945aecbdc97)

--- src/AvatarProvider.php
+++ src/AvatarProvider.php
@@ -47,7 +47,7 @@ class AvatarProvider implements IProfileImageProvider {
 	 */
 	private function generateFile( UserIdentity $user, array $params ) {
 		$file = $this->generator->getAvatarFile( $user );
-		if ( !$file->exists() ) {
+		if ( !$file || !$file->exists() ) {
 			$this->generator->generate( $user );
 		}
 
